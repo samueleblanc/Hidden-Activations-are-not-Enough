1f208fb22dbbe49e001989b585a2f596054f1133
samueleblanc <samueleblanc17@gmail.com>
2025-01-14 22:45:49 -0500

    refactor forward method

--- matrix_construction/representation.py
+++ matrix_construction/representation.py
@@ -80,83 +80,72 @@ class ConvRepresentation_2D:
         self.model = model
         self.channels = model.channels
         self.act_fn = model.get_activation_fn()
+        self.layers = list(model.modules())
         self.in_c, self.in_h, self.in_w = model.input_shape
         self.input_size: int = self.in_c*self.in_h*self.in_w
 
         self.current_output: CNN_2D|None = None  #Saves the output of the neural network on the current sample in the forward method
 
-        self.conv_layers: list[nn.Module] = []
         self.fc_layers: list[nn.Module] = []
 
-        for layer in model.modules():
-            if isinstance(layer, nn.Conv2d):
-                self.conv_layers.append(layer)
-            elif isinstance(layer, nn.Linear):
-                self.fc_layers.append(layer)
-            elif isinstance(layer, nn.AvgPool2d):
-                self.conv_layers.append(layer)
-            elif isinstance(layer, nn.BatchNorm2d):
-                NotImplementedError()  # TODO: Add support for BatchNorm
+        for i,layer in enumerate(self.layers):
+            if isinstance(layer, nn.Linear):
+                self.first_fc_layer = i
+                break
+        if self.model.bias:
+            for layer in self.layers:
+                if isinstance(layer, nn.Linear):
+                    self.fc_layers.append(layer)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # TODO: Parallelize the forward passes
         with torch.no_grad():
             self.model.save = True
             self.current_output = self.model(x, rep=True)  # Saves activations and preactivations
+            # TODO: We should be able to save activations and preacts for a NN that we get online
 
-            m = len(self.conv_layers)-1
             A = torch.Tensor()  # Will become M(W,f)(x)
             zeros = torch.zeros((1,self.in_c,self.in_h,self.in_w)).to(self.device)  # Input used to compute the columns of M(W,f)(x)
             for c in range(self.in_c):
                 for h in range(self.in_h):
                     for w in range(self.in_w):
-                        # First layer
                         zeros[0][c][h][w] = x[c][h][w].item()
-                        B = self.conv_layers[0](zeros).to(self.device)
-                        zeros[0][c][h][w] = 0.0
-                        
-                        # Conv layers
-                        for i in range(1, m):
+                        i = 0
+                        for j,layer in enumerate(self.layers):
                             pre_act = self.model.pre_acts[i-1]
                             post_act = self.model.acts[i-1]
                             vertices = post_act / pre_act
                             vertices[torch.isnan(vertices) | torch.isinf(vertices)] = 0.0
-                            B = B * vertices
-                            B = self.conv_layers[i](B)
-
-                        # Average pooling layer
-                        pre_act = self.model.pre_acts[m-1]
-                        post_act = self.model.acts[m-1]
-                        vertices = post_act / pre_act
-                        vertices[torch.isnan(vertices) | torch.isinf(vertices)] = 0.0
-                        B = B * vertices
-                        B = self.conv_layers[m](B)
-
-                        # First FC layer
-                        pre_act = self.model.pre_acts[m]
-                        post_act = self.model.acts[m]
-                        vertices = post_act / pre_act
-                        vertices[torch.isnan(vertices) | torch.isinf(vertices)] = 0.0
-                        B = B * vertices
-                        B = torch.matmul(self.fc_layers[0].weight.data, B.view(-1).unsqueeze(-1))
-
-                        # FC layers
-                        for i in range(1, len(self.fc_layers)):
-                            pre_act = self.model.pre_acts[m+i]
-                            post_act = self.model.acts[m+i]
-                            vertices = post_act / pre_act
-                            vertices[torch.isnan(vertices) | torch.isinf(vertices)] = 0.0
-                            B = B * vertices.unsqueeze(-1)
-                            B = torch.matmul(self.fc_layers[i].weight.data, B)
+                            if isinstance(layer, (nn.Conv2d, nn.AvgPool2d)):
+                                if i == 0:
+                                    B = layer(zeros).to(self.device)
+                                else:
+                                    B = B * vertices
+                                    B = layer(B)
+                                i += 1
+                            elif isinstance(layer, nn.Linear):
+                                if j == self.first_fc_layer:
+                                    if self.model.bias:
+                                        m = i
+                                    B = B * vertices
+                                    B = torch.matmul(layer.weight.data, B.view(-1).unsqueeze(-1))
+                                else:
+                                    B = B * vertices.unsqueeze(-1)
+                                    B = torch.matmul(layer.weight.data, B)
+                                i += 1
+                            elif isinstance(layer, nn.BatchNorm2d):
+                                i += 1
+                                NotImplementedError()
 
                         A = torch.cat((A,B),dim=-1)  # Cat the vector produced to the matrix M(W,f)(x)
+                        zeros[0][c][h][w] = 0.0
 
             if self.model.bias:
                 a = self.fc_layers[0].bias.data
 
                 for i in range(1, len(self.fc_layers)):  # FC layers
-                    pre_act = self.model.pre_acts[m+i]
-                    post_act = self.model.acts[m+i]
+                    pre_act = self.model.pre_acts[m+i-1]
+                    post_act = self.model.acts[m+i-1]
                     vertices = post_act / pre_act
                     vertices[torch.isnan(vertices) | torch.isinf(vertices)] = 0.0
                     B = self.fc_layers[i].weight.data * vertices.unsqueeze(0)
