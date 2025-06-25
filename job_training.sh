00693bbdd0fa7bd92d9900e5d2150a08d8b68296
Marco Antonio Armenta Armenta <armenta@beluga4.int.ets1.calculquebec.ca>
2025-06-25 15:16:21 -0400

    robust code avoid memory issues

--- job_training.sh
+++ job_training.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --account=def-assem  #account to charge the calculation
+#SBATCH --account=def-bruestle  #account to charge the calculation
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=10:00:00 #hour:minutes:seconds
+#SBATCH --time=12:00:00 #hour:minutes:seconds
 #SBATCH --mem=4G #memory requested
 
 module load StdEnv/2020 python/3.9.6 scipy-stack/2023a cuda cudnn #load the required module
@@ -13,5 +13,8 @@ echo "Copying datasets..."
 cp -r data/cifar-10-batches-py/* $SLURM_TMPDIR/data/cifar-10-batches-py/
 echo "data ready..."
 source env/bin/activate #load the virtualenv (absolute or relative path to where the script is submitted)
+
+export CUDA_LAUNCH_BLOCKING=1
+export TORCH_USE_CUDA_DSA=1
 
 python scratch-train.py
--- scratch-train.py
+++ scratch-train.py
@@ -8,13 +8,14 @@ import joblib
 #import certifi
 import os
 #from optuna.storages import JournalStorage, JournalFileBackend
-
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 from optuna.storages import JournalStorage
 from optuna.storages.journal import JournalFileBackend
 #os.environ['SSL_CERT_FILE'] = certifi.where()
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 device = 'cuda' if torch.cuda.is_available() else device
 print("Device: ", device, flush=True)
+num_gpus = torch.cuda.device_count()
 
 def train_model(trial):
     # Define hyperparameters to tune
@@ -33,31 +34,33 @@ def train_model(trial):
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
     # Load CIFAR10 dataset
-    trainset = datasets.CIFAR10(data_dir, download=True, train=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    trainset = datasets.CIFAR10(data_dir, download=False, train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, pin_memory=True)
 
-    testset = datasets.CIFAR10(data_dir, download=True, train=False, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    testset = datasets.CIFAR10(data_dir, download=False, train=False, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, pin_memory=True)
     #print("Data ready",flush=True)
     #return
     # Initialize VGG model
     model = VGG(input_shape=(3, 32, 32), num_classes=10, pretrained=False).to(device)
 
     # Define loss function and optimizer
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    model.train()
     # Train the model
     for epoch in range(epochs):
         #print("Epoch: ", epoch)
         for i, data in enumerate(trainloader, 0):
             inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
+            inputs, labels = inputs.to(device, non_blocking=True), labels.to(device, non_blocking=True)
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
+            torch.cuda.synchronize()
 
     # Evaluate the model on the validation set
     model.eval()
@@ -66,7 +69,7 @@ def train_model(trial):
     with torch.no_grad():
         for data in testloader:
             images, labels = data
-            images, labels = images.to(device), labels.to(device)
+            images, labels = images.to(device, non_blocking=True), labels.to(device, non_blocking=True)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
@@ -80,20 +83,20 @@ def save_study(study, trial):
     study_dir = "/lustre04/scratch/armenta"
     if not os.path.exists(study_dir):
         os.makedirs(study_dir)
-    joblib.dump(study, "vgg_hyperparameter_tuning_study.pkl")
+    joblib.dump(study, "vgg_hyperparameter_tuning_study_01.pkl")
 
 
 if __name__ == '__main__':
     # Create a study object and specify the direction
-    storage = JournalStorage(JournalFileBackend('/lustre04/scratch/armenta/vgg_journal.log'))
+    storage = JournalStorage(JournalFileBackend('/lustre04/scratch/armenta/vgg_journal_01.log'))
     study = optuna.create_study(direction="maximize",
-                                study_name="vgg_journal",
+                                study_name="vgg_journal_01",
                                 storage=storage,
                                 load_if_exists=True)
 
     # Perform hyperparameter tuning using Optuna
     study.optimize(train_model, n_trials=50,
-                                n_jobs=2, callbacks=[save_study])
+                                n_jobs=num_gpus, callbacks=[save_study])
     #study.optimize(train_model, n_trials=50)
 
     # Print the best hyperparameters and accuracy
